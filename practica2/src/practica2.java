abfb4757da283104af89376ac1520cd69050387e
Daniel <sittingbout@gmail.com>
2020-04-12 12:00:12 +0200

    probando git desde vscode

--- practica2/src/practica2.java
+++ practica2/src/practica2.java
@@ -14,7 +14,7 @@ public class practica2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("cosas");
     }
     
 }
