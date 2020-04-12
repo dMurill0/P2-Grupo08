29fb1286134cdd16164d50128f21ee25204ae344
sergiomartin <sergiomartin@mbp-de-sergio.home>
2020-04-12 12:42:34 +0200

    Commit Sergio: Clase Usuario (metodos)

--- practica2/src/Usuario.java
+++ practica2/src/Usuario.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,19 @@
  * @author sergiomartin
  */
 public class Usuario {
+    
+    private String nombre ;
+    private String apellidos;
+    private String contraseña;
+    private String email;
+    private ArrayList<String> notificaciones;
+ 
+    public void recibirNotificacion(String notificacion){
+        notificaciones.add(notificacion);
+    }
+
+    public ArrayList<String> getNotificaciones() {
+        return notificaciones;
+    }
     
 }
