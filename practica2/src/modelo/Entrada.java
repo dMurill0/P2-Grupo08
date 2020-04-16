1700c437869dab298cb340415f7632df5d27c02e
Alberto <foyoneroo@gmail.com>
2020-04-16 16:29:40 +0200

    Commit Alberto: Creación de la clase Subject. Creación de los métodos de las clases Sistema, Subforo y Subject. Prueba en la clase Demostrador.

--- practica2/src/modelo/Entrada.java
+++ practica2/src/modelo/Entrada.java
@@ -1,3 +1,5 @@
+package modelo;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +10,6 @@
  *
  * @author celia
  */
-public class Profesor extends Usuario{
+public class Entrada {
     
 }
