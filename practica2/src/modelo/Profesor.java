7ed0191a206fdfa5e3454efc08863eaef537318e
sergiomartin <sergiomartin@mbp-de-sergio.home>
2020-04-22 15:27:03 +0200

    Commit Sergio: 4

--- practica2/src/modelo/Profesor.java
+++ practica2/src/modelo/Profesor.java
@@ -1,6 +1,10 @@
 package modelo;
 
 public class Profesor extends Usuario{
+
+    public Profesor(String nombre, String apellidos, String nick, String contraseña, String email) {
+        super(nombre, apellidos, nick, contraseña, email);
+    }
     
     public void crearEncuesta(){
         Encuesta encuesta = new Encuesta();
