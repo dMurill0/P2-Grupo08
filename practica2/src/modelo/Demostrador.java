9dd7d8074033a9cf17bab33eed3ea4cad84cd646
Daniel <sittingbout@gmail.com>
2020-04-25 18:08:14 +0200

    demostrador

--- practica2/src/modelo/Demostrador.java
+++ practica2/src/modelo/Demostrador.java
@@ -9,44 +9,55 @@ public class Demostrador {
         System.out.println("Inicio del Sistema...");
         Sistema sesion = new Sistema();
         System.out.println("Sistema iniciado correctamente.");
-        System.out.println("Inicio de sesión con Usuario No registrado...");
-        System.out.println("");
-        System.out.println("");
-        /*               Login                     */
-        System.out.println("Iniciamos la sesión sin ningún usuario registrado");
-        if (sesion.login("dani", "danilo")){
-            System.out.println("Sesion iniciada correctamente "+ sesion.getUsuarioConectado().getNick());
-        }
-        else {
-            System.out.println("Error. Nick y/o password incorrectos");
-        }
-        /*               Logout                     */
+
+        /* Login */
+        login(sesion);
+        /* Registro */
+        registro(sesion);
+        /* Logout */
         sesion.logout();
+        /* Subforo */
+        System.out.println("El usuario " + sesion.getUsuarioConectado().getNick() + " va a crear un subforo");
+        if (sesion.getUsuarioConectado().getRol().equalsIgnoreCase("profesor")) {
 
-        /*               Registro                     */
-        System.out.println("Creación de una nueva cuenta");
-        if(sesion.registrarUsuario("Daniel", "Murillo Garzon", "cucu", "tras", "emilio@gmail.com")){
-            System.out.println("Bienvenido al Foro "+ sesion.getUsuarioConectado().getNick());
-        }
-        else{
-            System.out.println("Ha habido un error al crear la cuenta.");
-        }
-
-        /*               Subforo                     */
-        System.out.println("El usuario "+sesion.getUsuarioConectado().getNick()+ " va a crear un subforo");
-        if(sesion.getUsuarioConectado().getRol().equalsIgnoreCase("profesor")){
-            
-        }
-        else{
+        } else {
             System.out.println("Ha habido un error al crear el subforo. Debes ser profesor para poder crearlo");
         }
 
-        /*               Entrada                     */
+        /* Entrada */
 
-
-        /*               Persistencia de datos                     */
-
+        /* Persistencia de datos */
 
     }
-    
+
+    public static void login(Sistema sesion){
+        System.out.println("Iniciamos la sesión con  usuario no registrado");
+        if (sesion.login("cucu", "tras")) {
+            System.out.println("Sesion iniciada correctamente " + sesion.getUsuarioConectado().getNick());
+        } else {
+            System.out.println("Error. Nick y/o password incorrectos");
+        }
+        System.out.println("Iniciamos la sesión con  usuario no registrado");
+        if (sesion.login("dani", "tras")) {
+            System.out.println("Sesion iniciada correctamente " + sesion.getUsuarioConectado().getNick());
+        } else {
+            System.out.println("Error. Nick y/o password incorrectos");
+        }
+    }
+
+    public static void registro(Sistema sesion){
+        System.out.println("Registro con correo no perteneciente a la URJC");
+        if (sesion.registrarUsuario("Daniel", "Murillo Garzon", "cucu", "tras", "emilio@gmail.com")) {
+            System.out.println("Bienvenido al Foro " + sesion.getUsuarioConectado().getNick());
+        } else {
+            System.out.println("El correo es erroneo.");
+        }
+
+        System.out.println("Registro con correo perteneciente a la URJC");
+        if (sesion.registrarUsuario("Daniel", "Murillo Garzon", "cucu", "tras", "emilio@alumnos.urjc.es")) {
+            System.out.println("Bienvenido al Foro " + sesion.getUsuarioConectado().getNick());
+        } else {
+            System.out.println("El correo es erroneo.");
+        }
+    }
 }
