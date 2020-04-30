9ebc96b49df8c9eadd9a1f77b8f5b40946d69ee1
Fezzik23 <jokersm23@gmail.com>
2020-04-30 17:37:44 +0200

    Get Y set de Penalizacion en Alumno

--- practica2/src/modelo/usuario/Alumno.java
+++ practica2/src/modelo/usuario/Alumno.java
@@ -7,8 +7,17 @@ import modelo.sistema.Subforo;
 
 
 public class Alumno extends Usuario{
+    
     private Administrador administrador;
     private Penalizacion penaliz;
+
+    public Penalizacion getPenaliz() {
+        return penaliz;
+    }
+
+    public void setPenaliz(Penalizacion penaliz) {
+        this.penaliz = penaliz;
+    }
     
     public Alumno(String nombre, String apellidos, String nick, String contraseña, String email, Penalizacion penalizacion) {
         super(nombre, apellidos, nick, contraseña, email);
