7502e67bcaede7a51709d7b7e5a8f6fbe786d6fc
Daniel <sittingbout@gmail.com>
2020-05-13 17:25:09 +0200

    clase allTests

---
+++ practica2MP/test/modelo/AllTests.java
@@ -0,0 +1,21 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    AlumnoTest.class,
+    ProfesorTest.class,
+    AdministradorTest.class,
+    SubforoTest.class,
+    ComentarioTest.class,
+    EjercicioTest.class,
+    EncuestaTest.class,
+    EntradaTest.class,
+    TextoPlanoTest.class
+})
+
+
+public class AllTest {
+    
+}
